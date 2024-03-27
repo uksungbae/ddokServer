7749209c8d13272af00f2d7fb5e684c1a2d95c5b
배성욱 <143690265+uksungbae@users.noreply.github.com>
2024-03-27 20:21:12 +0900

    first commit

---
+++ ddokddok/src/main/java/com/example/ddokddok/DdokddokApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddokddok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdokddokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdokddokApplication.class, args);
+	}
+
+}
---
+++ ddokddok/src/test/java/com/example/ddokddok/DdokddokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddokddok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdokddokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
